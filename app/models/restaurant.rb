69b442586e1e4aa8e2f258607fd80029bbaa62b2
Andrey Ekimenko <andrey.n.ekimenko@gmail.com>
2016-08-23 23:08:34 +0300

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{2}-\d{2}\z/,
+    	message: "must be in the format (123) 456-78-90" }
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    	message: "must start with http:// or https://" }
+    validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    	message: "must in the format 350 Fifth Avenue, New York, NY 10118" }
 end
